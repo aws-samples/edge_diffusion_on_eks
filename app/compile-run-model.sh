4db817c9df605047661a70c964f023866f0575fa
yahavb <yahavb@amazon.com>
2023-11-21 09:51:07 -0800

    pipeline

--- app/compile-run-model.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash -x
-
-aws s3 cp s3://sdinfer/sd_1_5_fp32_512_compile_workdir.tar.gz /
-tar -xvf /sd_1_5_fp32_512_compile_workdir.tar.gz
-
-. aws_neuron_venv_pytorch_inf/bin/activate
-#time python /compile-neuron-model.py
-time python /run-neuro-model.py
-deactivate
