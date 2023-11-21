37549a9ecae188f5ceeb67d5d2b47d0d463b45da
yahavb <yahavb@amazon.com>
2023-11-21 14:28:46 -0800

    compile-neuron-model.sh

--- app/compile-neuron-model.sh
+++ app/compile-neuron-model.sh
@@ -2,8 +2,9 @@
 
 #delete the below after validating mulit-stage docker image works
 #aws s3 cp s3://sdinfer/sd_1_5_fp32_512_compile_workdir.tar.gz /
-#tar -xvf /sd_1_5_fp32_512_compile_workdir.tar.gz
 
 . aws_neuron_venv_pytorch_inf/bin/activate
 time python /compile-neuron-model.py 
 deactivate
+tar -czvf /sd_1_5_fp32_512_compile_workdir.tar.gz /sd_1_5_fp32_512_compile_workdir/
+aws s3 cp /sd_1_5_fp32_512_compile_workdir.tar.gz s3://sdinfer/sd_1_5_fp32_512_compile_workdir.tar.gz
