7d017cac501d20571cb1d99449a0f82d08219dc2
yahavb <yahavb@amazon.com>
2023-11-20 16:32:28 -0800

    build-assets.sh

--- app/build-assets.sh
+++ app/build-assets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
 
-ASSETS="-assets"
+ASSETS="-mlassets"
 export IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$IMAGE_TAG$ASSETS
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $IMAGE
 docker build -t $IMAGE ./Dockerfile-assets
