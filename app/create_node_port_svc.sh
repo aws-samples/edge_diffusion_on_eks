a8dbda93ddc47193c33571c481e2d1982230ccfd
yahavb <yahavb@amazon.com>
2023-11-21 14:57:30 -0800

    remove -x

--- app/create_node_port_svc.sh
+++ app/create_node_port_svc.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -x
+#!/bin/bash
 kubectl label pod $POD_NAME inferencepod=$POD_NAME
 service_name=$POD_NAME-svc-$(kubectl get no -o wide `kubectl  get po  $POD_NAME -o wide | awk '{print $7}'|grep -v NODE`| awk '{print $7}' | grep -v EXTERNAL-IP|sed "s/\./-/g")
 export SVC_NAME=$service_name
