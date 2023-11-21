8ac8fe501143ea18e1d7dd447fd2f0c888bd6ebf
yahavb <yahavb@amazon.com>
2023-11-21 10:48:55 -0800

    build.sh

--- app/build.sh
+++ app/build.sh
@@ -3,8 +3,10 @@
 ASSETS="-assets"
 export BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$BASE_IMAGE_TAG
 export ASSETS_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$IMAGE_TAG$ASSETS
+export IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$IMAGE_TAG
 
 cat Dockerfile.template | envsubst > Dockerfile
+cat Dockerfile
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $BASE_IMAGE
 docker build -t $IMAGE .
 docker push $IMAGE
