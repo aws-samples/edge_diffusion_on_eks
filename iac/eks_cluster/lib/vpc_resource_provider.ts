854148556cbfb20dc692bd585882e64639f4e33e
Serge Poueme <pouemes@amazon.com>
2023-12-20 10:53:58 -0500

    refactored

--- iac/eks_cluster/lib/vpc_resource_provider.ts
+++ iac/eks_cluster/lib/vpc_resource_provider.ts
@@ -15,12 +15,13 @@ export class VpcResourceProvider implements ResourceProvider<IVpc> {
     provide(context: ResourceContext): IVpc {
         return new ec2.Vpc(context.scope, 'eks-edge-vpc', {
             ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
-            availabilityZones: ['us-west-2-lax-1a', 'us-west-2a'], // VPC spans all AZs
+            natGateways: 0,
+            availabilityZones: ['us-west-2-lax-1a','us-west-2-lax-1b'], // VPC spans all AZs
             subnetConfiguration: [ {
                 cidrMask: 26,
                 name: 'public-eks-subnet',
                 subnetType: SubnetType.PUBLIC,
-                mapPublicIpOnLaunch: true
+                //mapPublicIpOnLaunch: true
             },
                 {
                     cidrMask: 26,
@@ -28,59 +29,7 @@ export class VpcResourceProvider implements ResourceProvider<IVpc> {
                     subnetType: SubnetType.PRIVATE_WITH_EGRESS
                 },
 
-            ],
-            natGatewaySubnets: {
-                availabilityZones: ['us-west-2-lax-1a'], // NAT gateway only in 1 AZ
-                subnetType: SubnetType.PUBLIC
-            }
+            ]
         });
     }
 }
-
-
-/*export class VPCStack extends Stack {
-    public readonly vpc: ec2.Vpc;
-
-    get availabilityZones() {
-        return [
-            PARENT_REGION_AZ,
-            LOCAL_ZONE_AZ,
-        ];
-    }
-
-    constructor(scope: App, id: string, props?: StackProps) {
-        super(scope, id, props);
-
-        this.vpc = new ec2.Vpc(this, 'edge-cluster-vpc', {
-            ipAddresses: ec2.IpAddresses.cidr(VPC_CIDR),
-            vpcName: 'edge-cluster-vpc',
-            subnetConfiguration: [
-                {
-                    cidrMask: SUBNET_SIZE,
-                    name: 'public-subnet-lz',
-                    subnetType: ec2.SubnetType.PUBLIC,
-                    availabilityZones: [LOCAL_ZONE_AZ],
-                    mapPublicIpOnLaunch: true,
-
-                },
-                {
-                    cidrMask: SUBNET_SIZE,
-                    name: 'public-subnet-ir',
-                    subnetType: ec2.SubnetType.PUBLIC,
-                    availabilityZones: [PARENT_REGION_AZ],
-                    mapPublicIpOnLaunch: true
-                }
-            ]
-
-        });
-
-        this.vpc.publicSubnet.addTags({
-            'kubernetes.io/cluster/eks-edge-cluster': 'shared',
-            'kubernetes.io/role/elb': '1'
-        });
-
-
-
-
-    }
-}*/
