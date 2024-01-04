ddde1e101fa985e6a2f4a83ed961139455463608
yahavb <yahavb@amazon.com>
2024-01-04 12:30:52 -0800

    model-param

--- app/compile-neuron-model.sh
+++ app/compile-neuron-model.sh
@@ -3,6 +3,6 @@
 . aws_neuron_venv_pytorch_inf/bin/activate
 time python /compile-neuron-model.py 
 deactivate
-tar -czvf /sd2_compile_dir/${MODEL_FILE}.tar.gz /sd2_compile_dir/${MODEL_FILE}/
+tar -czvf /sd2_compile_dir/${MODEL_FILE}.tar.gz /sd2_compile_dir/
 aws s3 cp /sd2_compile_dir/${MODEL_FILE}.tar.gz s3://sdinfer/{MODEL_FILE}_xla.tar.gz
 aws s3api put-object-acl --bucket sdinfer --key /{MODEL_FILE}_xla.tar.gz --acl public-read
