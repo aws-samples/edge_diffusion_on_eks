ea60a4d105bf729e2b0e49d41a4ffe4ab39b74a9
yahavb <yahavb@amazon.com>
2024-01-05 11:20:07 -0800

    run-neuron-model.sh

--- app/run-neuron-model.sh
+++ app/run-neuron-model.sh
@@ -1,5 +1,7 @@
 #!/bin/bash -x
 
+tar -xvzf /app/model.tar.gz 
+
 . aws_neuron_venv_pytorch_inf/bin/activate
 if [[ $MODEL_FILE == "stable-diffusion-2-1-base" ]]; then
   python /run-sd21-neuron-model.py
