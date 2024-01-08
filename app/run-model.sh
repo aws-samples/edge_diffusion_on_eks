4166203ca1aa6acbb23bb816a1994de99e74d73c
yahavb <yahavb@amazon.com>
2024-01-07 17:40:57 -0800

    runmodel

---
+++ app/run-model.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+
+tar -xvzf /app/model.tar.gz 
+
+. aws_${DEVICE}_venv_pytorch/bin/activate
+if [[ $MODEL_FILE == "stable-diffusion-2-1-base" ]]; then
+  python /sd2_512_benchmark.py
+fi
+deactivate
+while true; do sleep 1000; done
