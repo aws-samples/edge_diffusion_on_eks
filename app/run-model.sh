e5243df224245fe7b48aa6f1217257d53c2393ca
Yahav Biran <yahavb@me.com>
2024-01-09 15:54:24 -0800

    disable venv with dlc

--- app/run-model.sh
+++ app/run-model.sh
@@ -2,9 +2,9 @@
 
 tar -xvzf /app/model.tar.gz 
 
-. aws_${DEVICE}_venv_pytorch/bin/activate
+#. aws_${DEVICE}_venv_pytorch/bin/activate
 if [[ $MODEL_FILE == "stable-diffusion-2-1-base" ]]; then
   python /sd2_512_benchmark.py
 fi
-deactivate
+#deactivate
 while true; do sleep 1000; done
