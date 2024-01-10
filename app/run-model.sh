6f7dfa995475b769bdd932ad81d93c87d78afbf0
Yahav Biran <yahavb@me.com>
2024-01-09 21:03:23 -0800

    readd venv with dlc

--- app/run-model.sh
+++ app/run-model.sh
@@ -4,6 +4,7 @@ tar -xzf /app/model.tar.gz
 python -m venv aws_${DEVICE}_venv_pytorch
 if [ "$(uname -i)" = "x86_64" ]; then
   . aws_${DEVICE}_venv_pytorch/bin/activate
+  pip install --upgrade pip
   if [ $DEVICE="xla" ]; then
     pip install diffusers==0.20.2 transformers==4.33.1 accelerate==0.22.0 safetensors==0.3.1 matplotlib Pillow ipython -U      
   fi
