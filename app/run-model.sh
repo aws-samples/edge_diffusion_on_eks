86327c8d0d54918125df8b3a76cdcba544633436
Yahav Biran <yahavb@me.com>
2024-01-09 20:36:25 -0800

    readd venv with dlc

--- app/run-model.sh
+++ app/run-model.sh
@@ -1,14 +1,13 @@
 #!/bin/bash -x
 
 tar -xzf /app/model.tar.gz 
-
+python -m venv aws_${DEVICE}_venv_pytorch
 if [ "$(uname -i)" = "x86_64" ]; then
+  . aws_${DEVICE}_venv_pytorch/bin/activate
   if [ $DEVICE="xla" ]; then
     pip install diffusers==0.20.2 transformers==4.33.1 accelerate==0.22.0 safetensors==0.3.1 matplotlib Pillow ipython -U      
-    python /sd2_512_benchmark.py
   fi
+  python /sd2_512_benchmark.py
+  deactivate
 fi
-#. aws_${DEVICE}_venv_pytorch/bin/activate
-#if [[ $MODEL_FILE == "stable-diffusion-2-1-base" ]]; then
-#deactivate
 while true; do sleep 1000; done
