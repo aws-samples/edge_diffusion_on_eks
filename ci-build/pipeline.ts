c479a99b593a6a4077a24991574fc06d34e13bc9
Yahav Biran <yahavb@me.com>
2024-01-17 10:48:15 -0800

    infra-build

--- ci-build/pipeline.ts
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { PipelineStack } from './pipeline-stack';
-
-const app = new cdk.App();
-new PipelineStack(app, 'PipelineStack', {
-  env: { account: process.env.AWS_ACCOUNT_ID, region: process.env.AWS_REGION},
-});
