a7d59d4d9acf77ce6a26c880bc46e8147264caf2
Serge Poueme <10876571+sdpoueme@users.noreply.github.com>
2024-01-21 22:15:27 -0500

    Update vpc-resource-provider.ts

--- infra-build/vpc-resource-provider.ts
+++ infra-build/vpc-resource-provider.ts
@@ -1,27 +1,28 @@
-import {Stack,StackProps, App} from 'aws-cdk-lib';
+import {Stack, StackProps, App} from 'aws-cdk-lib';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import {ResourceContext, ResourceProvider} from "@aws-quickstart/eks-blueprints";
 import {IVpc, SubnetType} from "aws-cdk-lib/aws-ec2";
 
+
 export class VpcResourceProvider implements ResourceProvider<IVpc> {
+
     provide(context: ResourceContext): IVpc {
-        return new ec2.Vpc(context.scope, 'vpc', {
+        return new ec2.Vpc(context.scope, 'edge-eks-vpc', {
             ipAddresses: ec2.IpAddresses.cidr('10.0.0.0/16'),
             natGateways: 0,
-            //availabilityZones: ['us-west-2-lax-1a','us-west-2d','us-west-2b'], 
-            availabilityZones: ['us-west-2d','us-west-2b'], 
-            subnetConfiguration: [ 
-              {
+            availabilityZones: ['us-west-2-lax-1a', 'us-west-2-lax-1b', 'us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d'], // VPC spans all AZs
+            subnetConfiguration: [{
                 cidrMask: 26,
-                name: 'public',
+                name: 'public-eks-subnet',
                 subnetType: SubnetType.PUBLIC,
-                //mapPublicIpOnLaunch: true
-              },
-              {
-                cidrMask: 26,
-                name: 'private',
-                subnetType: SubnetType.PRIVATE_WITH_EGRESS
-              },
+                mapPublicIpOnLaunch: true
+            },
+                {
+                    cidrMask: 26,
+                    name: 'private-eks-subnet',
+                    subnetType: SubnetType.PRIVATE_WITH_EGRESS
+                },
+
             ]
         });
     }
