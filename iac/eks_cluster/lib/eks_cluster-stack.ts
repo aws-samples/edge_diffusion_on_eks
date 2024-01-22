5f344f608569131d3c7e020a52e43b2729f7d68b
Serge Poueme <pouemes@amazon.com>
2024-01-21 22:18:32 -0500

    refactored to solve deployment issues

--- iac/eks_cluster/lib/eks_cluster-stack.ts
+++ iac/eks_cluster/lib/eks_cluster-stack.ts
@@ -2,12 +2,15 @@ import * as cdk from 'aws-cdk-lib';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
 import {GlobalResources} from '@aws-quickstart/eks-blueprints';
 import {VpcResourceProvider} from './region_vpc_resource_provider';
-import {CapacityType, KubernetesVersion, MachineImageType} from 'aws-cdk-lib/aws-eks';
+import {EndpointAccess, MachineImageType} from 'aws-cdk-lib/aws-eks';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
+import * as iam from "aws-cdk-lib/aws-iam";
 import {SubnetType} from "aws-cdk-lib/aws-ec2";
 
-const version = 'auto';
-const cluster_name = 'edge-inference-cluster3';
+
+const version = "auto";
+const cluster_name = 'edge-eks-cluster';
+
 
 export class EksClusterStack extends cdk.Stack {
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -16,61 +19,58 @@ export class EksClusterStack extends cdk.Stack {
         const account = this.account;
         const region = this.region;
 
-        //const edgeVPC = new VpcResourceProvider();
+
         const regionVPC = new VpcResourceProvider();
 
         const addOns: Array<blueprints.ClusterAddOn> = [
             new blueprints.addons.MetricsServerAddOn(),
-            new blueprints.addons.ClusterAutoScalerAddOn(),
             new blueprints.addons.AwsLoadBalancerControllerAddOn(),
             new blueprints.addons.VpcCniAddOn(),
-            new blueprints.addons.CoreDnsAddOn(),
-            new blueprints.addons.KubeProxyAddOn()
+            new blueprints.addons.CoreDnsAddOn()
         ];
 
-
-
-        /*const coreNodesProvider = new blueprints.MngClusterProvider(
+        const nodesProvider = new blueprints.GenericClusterProvider(
             {
-                 id: "core",
-                 version: KubernetesVersion.of("auto"),
-                 desiredSize: 3,
-                 nodeGroupCapacityType: CapacityType.ON_DEMAND,
-                 minSize: 1,
-                 maxSize: 3,
-                 nodeGroupSubnets: { availabilityZones: ['us-west-2a', 'us-west-2b'] },
-                 clusterName: `${cluster_name}`
-             }
-         )*/
+                clusterName: cluster_name,
+                vpcSubnets: [{availabilityZones: ['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d']}],
+                endpointAccess: EndpointAccess.PUBLIC,
+                //placeClusterHandlerInVpc: true,
+                autoscalingNodeGroups: [
+                    {
+                        id: "core",
+                        autoScalingGroupName: "core-nodes-asg",
+                        desiredSize: 3,
+                        minSize: 1,
+                        maxSize: 3,
+                        machineImageType: MachineImageType.AMAZON_LINUX_2,
+                        nodeGroupSubnets: {availabilityZones: ['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d']}
+                    },
+                    {
+                        id: "inference",
+                        autoScalingGroupName: "inference-nodes-asg",
+                        instanceType: new ec2.InstanceType('g4dn.2xlarge'),
+                        desiredSize: 3,
+                        minSize: 1,
+                        maxSize: 3,
+                        machineImageType: MachineImageType.AMAZON_LINUX_2,
+                        nodeGroupSubnets: {availabilityZones: ['us-west-2-lax-1a', 'us-west-2-lax-1b']}
 
-        const edgeNodesProvider = new blueprints.AsgClusterProvider(
-            {
-                id: "inference",
-                //vpc: edgeVPC.provide().vpcId,
-                version: KubernetesVersion.of("auto"),
-                desiredSize: 3,
-                minSize: 1,
-                maxSize: 3,
-                machineImageType: MachineImageType.AMAZON_LINUX_2,
-                instanceType: new ec2.InstanceType('g5.4xlarge'),
-                //nodeGroupSubnets: { availabilityZones: ['us-west-2-lax-1a','us-west-2-lax-1b'] },
-                autoScalingGroupName: "edge-nodes-asg",
-                clusterName: `${cluster_name}`,
-                vpcSubnets: [ {subnetType:SubnetType.PUBLIC }],
-
+                    }
+                ]
             }
         );
 
         const stack = blueprints.EksBlueprint.builder()
             .resourceProvider(GlobalResources.Vpc, regionVPC)
-            //.clusterProvider(coreNodesProvider)
-            .clusterProvider(edgeNodesProvider)
+            .clusterProvider(nodesProvider)
             .account(account)
             .region(region)
             .version(version)
+            .teams()
             .addOns(...addOns)
             .useDefaultSecretEncryption(false) // set to false to turn secret encryption off (non-production/demo cases)
             .build(this, cluster_name);
+
 
     }
 }
