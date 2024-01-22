da305c49e30ee1f8fae4982d5279dfa1ea4bad1f
Serge Poueme <pouemes@amazon.com>
2024-01-22 14:14:16 -0500

    refactored code to include all recents fixes and working packages

--- infra-build/eks-cluster-stack.ts
+++ infra-build/eks-cluster-stack.ts
@@ -1,17 +1,15 @@
 import * as cdk from 'aws-cdk-lib';
 import * as blueprints from '@aws-quickstart/eks-blueprints';
-import { GlobalResources } from "@aws-quickstart/eks-blueprints";
-import { VpcResourceProvider } from "./vpc-resource-provider";
+import {GlobalResources} from '@aws-quickstart/eks-blueprints';
+import {VpcResourceProvider} from './vpc-resource-provider';
+import {EndpointAccess, MachineImageType} from 'aws-cdk-lib/aws-eks';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as eks from "aws-cdk-lib/aws-eks";
-import * as iam from "aws-cdk-lib/aws-iam";
-import {UpdatePolicy} from "aws-cdk-lib/aws-autoscaling";
-import {SubnetFilter,SubnetType,InstanceType} from "aws-cdk-lib/aws-ec2";
-import {CapacityType,KubernetesVersion,MachineImageType} from 'aws-cdk-lib/aws-eks';
-import {AccountRootPrincipal} from "aws-cdk-lib/aws-iam";
+import {SubnetFilter, SubnetType} from 'aws-cdk-lib/aws-ec2';
 
-const version = 'auto';
-let cluster_name = process.env.CLUSTER as string;
+
+const version = "auto";
+const cluster_name = 'edge-eks-cluster';
+
 
 export class EksClusterStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
@@ -20,57 +18,62 @@ export class EksClusterStack extends cdk.Stack {
     const account = this.account;
     const region = this.region;
 
-    const clusterVPC = new VpcResourceProvider();
+
+    const regionVPC = new VpcResourceProvider();
 
     const addOns: Array<blueprints.ClusterAddOn> = [
       new blueprints.addons.MetricsServerAddOn(),
       new blueprints.addons.AwsLoadBalancerControllerAddOn(),
       new blueprints.addons.VpcCniAddOn(),
       new blueprints.addons.CoreDnsAddOn(),
-      new blueprints.addons.KubeProxyAddOn(),
-      new blueprints.addons.GpuOperatorAddon()
+      new blueprints.addons.GpuOperatorAddon(),
+
     ];
 
     const nodesProvider = new blueprints.GenericClusterProvider(
         {
-          clusterName: `${cluster_name}`,
-          mastersRole: blueprints.getResource(context => {
-                    return new iam.Role(context.scope, 'AdminRole', { assumedBy: new AccountRootPrincipal() });
-          }),
-          vpcSubnets: [{ availabilityZones: ['us-west-2a','us-west-2b','us-west-2c','us-west-2d'] }],
+          clusterName: cluster_name,
+          vpcSubnets: [{availabilityZones: ['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d']}],
+          endpointAccess: EndpointAccess.PUBLIC,
           autoscalingNodeGroups: [
             {
               id: "core",
-              autoScalingGroupName: "core",
-              allowAllOutbound: true,
+              autoScalingGroupName: "core-nodes-asg",
+              desiredSize: 3,
               minSize: 1,
               maxSize: 3,
+              allowAllOutbound: true,
               machineImageType: MachineImageType.AMAZON_LINUX_2,
               nodeGroupSubnets: {subnetType: SubnetType.PUBLIC ,subnetFilters: [SubnetFilter.availabilityZones(['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d'])]},
             },
             {
-              id: "edge",
-              autoScalingGroupName: "edge",
+              id: "inference",
+              autoScalingGroupName: "inference-nodes-asg",
+              instanceType: new ec2.InstanceType('g4dn.2xlarge'),
+              allowAllOutbound: true,
+              associatePublicIpAddress: true,
+              desiredSize: 3,
               minSize: 1,
               maxSize: 3,
               machineImageType: MachineImageType.AMAZON_LINUX_2,
-              nodeGroupSubnets: {subnetType: SubnetType.PUBLIC ,subnetFilters: [SubnetFilter.availabilityZones(['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d'])]},
-              //instanceType: new ec2.InstanceType('g5.4xlarge'),
-              //nodeGroupSubnets: {availabilityZones: ['us-west-2-lax-1a']},
+              //nodeGroupSubnets: {availabilityZones: ['us-west-2-lax-1a', 'us-west-2-lax-1b'],subnetFilters: [SubnetFilter.availabilityZones(['us-west-2-lax-1a', 'us-west-2-lax-1b'])]},
+              nodeGroupSubnets: { subnetType: SubnetType.PUBLIC,subnetFilters: [SubnetFilter.availabilityZones(['us-west-2-lax-1a', 'us-west-2-lax-1b'])]},
+
             }
           ]
         }
-    )
+    );
 
     const stack = blueprints.EksBlueprint.builder()
-        .resourceProvider(GlobalResources.Vpc, clusterVPC)
+        .resourceProvider(GlobalResources.Vpc, regionVPC)
         .clusterProvider(nodesProvider)
         .account(account)
         .region(region)
         .version(version)
-        .addOns(...addOns)
         .teams()
-        .useDefaultSecretEncryption(false) //false to turn secret encryption off (demo cases)
+        .addOns(...addOns)
+        .useDefaultSecretEncryption(false) // set to false to turn secret encryption off (non-production/demo cases)
         .build(this, cluster_name);
+
   }
 }
