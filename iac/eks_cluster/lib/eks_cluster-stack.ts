87438d41aebac535f5c9f4b9da92dee7f6ee2de3
Serge Poueme <pouemes@amazon.com>
2024-01-21 23:26:51 -0500

    refactored to allow the nodes to join the cluster

--- iac/eks_cluster/lib/eks_cluster-stack.ts
+++ iac/eks_cluster/lib/eks_cluster-stack.ts
@@ -4,8 +4,7 @@ import {GlobalResources} from '@aws-quickstart/eks-blueprints';
 import {VpcResourceProvider} from './region_vpc_resource_provider';
 import {EndpointAccess, MachineImageType} from 'aws-cdk-lib/aws-eks';
 import * as ec2 from 'aws-cdk-lib/aws-ec2';
-import * as iam from "aws-cdk-lib/aws-iam";
-import {SubnetType} from "aws-cdk-lib/aws-ec2";
+import {SubnetFilter, SubnetType} from 'aws-cdk-lib/aws-ec2';
 
 
 const version = "auto";
@@ -34,7 +33,6 @@ export class EksClusterStack extends cdk.Stack {
                 clusterName: cluster_name,
                 vpcSubnets: [{availabilityZones: ['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d']}],
                 endpointAccess: EndpointAccess.PUBLIC,
-                //placeClusterHandlerInVpc: true,
                 autoscalingNodeGroups: [
                     {
                         id: "core",
@@ -42,18 +40,22 @@ export class EksClusterStack extends cdk.Stack {
                         desiredSize: 3,
                         minSize: 1,
                         maxSize: 3,
+                        allowAllOutbound: true,
                         machineImageType: MachineImageType.AMAZON_LINUX_2,
-                        nodeGroupSubnets: {availabilityZones: ['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d']}
+                        nodeGroupSubnets: {subnetType: SubnetType.PUBLIC ,subnetFilters: [SubnetFilter.availabilityZones(['us-west-2a', 'us-west-2b', 'us-west-2c', 'us-west-2d'])]},
                     },
                     {
                         id: "inference",
                         autoScalingGroupName: "inference-nodes-asg",
                         instanceType: new ec2.InstanceType('g4dn.2xlarge'),
+                        allowAllOutbound: true,
+                        associatePublicIpAddress: true,
                         desiredSize: 3,
                         minSize: 1,
                         maxSize: 3,
                         machineImageType: MachineImageType.AMAZON_LINUX_2,
-                        nodeGroupSubnets: {availabilityZones: ['us-west-2-lax-1a', 'us-west-2-lax-1b']}
+                        //nodeGroupSubnets: {availabilityZones: ['us-west-2-lax-1a', 'us-west-2-lax-1b'],subnetFilters: [SubnetFilter.availabilityZones(['us-west-2-lax-1a', 'us-west-2-lax-1b'])]},
+                        nodeGroupSubnets: { subnetType: SubnetType.PUBLIC,subnetFilters: [SubnetFilter.availabilityZones(['us-west-2-lax-1a', 'us-west-2-lax-1b'])]},
 
                     }
                 ]
@@ -70,7 +72,6 @@ export class EksClusterStack extends cdk.Stack {
             .addOns(...addOns)
             .useDefaultSecretEncryption(false) // set to false to turn secret encryption off (non-production/demo cases)
             .build(this, cluster_name);
-
 
     }
 }
