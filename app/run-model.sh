32422f8815d4c4790330a5810302cfb6907048cd
Yahav Biran <yahavb@me.com>
2024-02-16 12:39:44 -0800

    diffusion_benchmarker-0.0.1.tar.gz

--- app/run-model.sh
+++ app/run-model.sh
@@ -2,12 +2,12 @@
 
 pip install --upgrade pip
 if [ "$(uname -i)" = "x86_64" ]; then
-  if [ $DEVICE="xla" ]; then
+  if [ "$DEVICE" == "xla" ]; then
     pip install diffusers==0.20.2 transformers==4.33.1 accelerate==0.22.0 safetensors==0.3.1 matplotlib Pillow ipython -U      
     pip install click
     pip install /diffusion_benchmarker-0.0.1.tar.gz 
     diffusion_benchmarker t2i --pipeline neuronx_t2i root=/app/sd2_compile_dir_512 pretrained_model_name_or_path=stabilityai/stable-diffusion-2-1 torch_dtype=bfloat16
-  elif [ $DEVICE="cuda" ]; then
+  elif [ "$DEVICE" == "cuda" ]; then
     pip install environment_kernels
     pip install diffusers transformers accelerate safetensors matplotlib Pillow ipython torch -U
     pip install click
