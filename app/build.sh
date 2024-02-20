1a803cd4827a69824684b5131db127ced0079572
Yahav Biran <yahavb@me.com>
2024-02-20 12:25:42 -0800

    dlc-neuron-2.17

--- app/build.sh
+++ app/build.sh
@@ -14,6 +14,9 @@ export BASE_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$
 export ASSETS_IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$IMAGE_TAG$ASSETS
 export IMAGE=$AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:$IMAGE_TAG
 
+docker tag $dlc_image_id $IMAGE
+docker images
+
 cat Dockerfile.template | envsubst > Dockerfile
 cat Dockerfile
 aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $BASE_IMAGE
