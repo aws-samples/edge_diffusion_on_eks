27c7da6f0bfb9c0f8245ac12306bbf2101d39375
Yahav Biran <yahavb@me.com>
2024-02-20 12:08:00 -0800

    dlc-neuron-2.17

--- app/build.sh
+++ app/build.sh
@@ -1,5 +1,12 @@
 #!/bin/bash -x
 
+docker logout
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin 763104351884.dkr.ecr.us-west-2.amazonaws.com
+docker pull 763104351884.dkr.ecr.us-west-2.amazonaws.com/pytorch-inference-neuronx:1.13.1-neuronx-py310-sdk2.17.0-ubuntu20.04
+dlc_image_id=$(docker images | grep 763104351884 | grep 1.13.1-neuronx-py310-sdk2.17.0-ubuntu20.04 | awk '{print $3}')
+docker images
+docker logout
+
 export AWS_ACCOUNT_ID=$(aws sts get-caller-identity --output text --query Account)
 
 ASSETS="-assets"
