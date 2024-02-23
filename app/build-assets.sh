be3d132aea16f084e39cf0cdfc7b448148691e69
Yahav Biran <yahavb@me.com>
2024-02-22 22:03:35 -0800

    dlc gpu and neuron

--- app/build-assets.sh
+++ app/build-assets.sh
@@ -5,6 +5,8 @@ if [ "$IMAGE_TAG" == "amd64-neuron" ]; then
   docker pull 763104351884.dkr.ecr.us-west-2.amazonaws.com/pytorch-inference-neuronx:1.13.1-neuronx-py310-sdk2.17.0-ubuntu20.04
   dlc_xla_image_id=$(docker images | grep 763104351884 | grep 1.13.1-neuronx-py310-sdk2.17.0-ubuntu20.04 | awk '{print $3}')
   docker tag $dlc_xla_image_id $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:1.13.1-neuronx-py310-sdk2.17.0-ubuntu20.04
+  docker logout
+  aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO
   docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:1.13.1-neuronx-py310-sdk2.17.0-ubuntu20.04
 fi
 if [ "$IMAGE_TAG" == "amd64-cuda" ]; then
@@ -12,6 +14,8 @@ if [ "$IMAGE_TAG" == "amd64-cuda" ]; then
   docker pull 763104351884.dkr.ecr.us-east-1.amazonaws.com/pytorch-inference:2.0.1-gpu-py310-cu118-ubuntu20.04-ec2
   dlc_cuda_image_id=$(docker images | grep 763104351884 | grep 2.0.1-gpu-py310-cu118-ubuntu20.04-ec2 | awk '{print $3}')
   docker tag $dlc_cuda_image_id $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:2.0.1-gpu-py310-cu118-ubuntu20.04-ec2
+  docker logout
+  aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO
   docker push $AWS_ACCOUNT_ID.dkr.ecr.$AWS_REGION.amazonaws.com/$BASE_REPO:2.0.1-gpu-py310-cu118-ubuntu20.04-ec2
 fi
 docker images
