c180e46a3f2785ff668266654f04385653a4c87e
Yahav Biran <yahavb@me.com>
2024-04-20 17:38:14 -0700

    trying optimum-neuron with distilbert-base-uncased-finetuned-sst-2-english

--- app/optimum-neuron.sh
+++ app/optimum-neuron.sh
@@ -3,7 +3,7 @@
 pip install --upgrade pip
 pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
 pip install --upgrade-strategy eager optimum[neuronx]
-optimum-cli export neuron 
+optimum-cli export neuron \
   --model distilbert-base-uncased-finetuned-sst-2-english \
   --batch_size 1 \
   --sequence_length 32 \
