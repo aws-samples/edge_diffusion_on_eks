ac6e63ee1d3ed62b87ee671b4f488e116ff94b4b
Yahav Biran <yahavb@me.com>
2024-04-21 10:39:03 -0700

    uninstall aws-cli

--- app/optimum-neuron-sd2-compile.sh
+++ app/optimum-neuron-sd2-compile.sh
@@ -3,6 +3,9 @@
 pip install --upgrade pip
 pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
 pip install "optimum[neuronx, diffusers]"
+pip uninstall awscli -y
+pip uninstall botocore s3transfer -y
+pip install awscli
 optimum-cli export neuron \
   --model $MODEL_ID \
   --task stable-diffusion \
