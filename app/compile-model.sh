e7a1fb3774ff97de98b84245ad529ddc8d124475
Yahav Biran <yahavb@me.com>
2024-04-21 00:06:21 -0700

    batch-size-in-model-file-name

--- app/compile-model.sh
+++ app/compile-model.sh
@@ -7,5 +7,5 @@ if [ "$(uname -i)" = "x86_64" ]; then
   fi
 fi
 tar -czvf /${COMPILER_WORKDIR_ROOT}/${MODEL_FILE}.tar.gz /${COMPILER_WORKDIR_ROOT}/
-aws s3 cp /${COMPILER_WORKDIR_ROOT}/${MODEL_FILE}.tar.gz s3://${BUCKET}/${MODEL_FILE}_${DEVICE}.tar.gz
+aws s3 cp /${COMPILER_WORKDIR_ROOT}/${MODEL_FILE}.tar.gz s3://${BUCKET}/${MODEL_FILE}_${DEVICE}_bsize_${BATCH_SIZE}.tar.gz
 while true; do sleep 1000; done
