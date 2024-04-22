813044fd62904e0dc6f9a91db1d494c6a64b1f81
Yahav Biran <yahavb@me.com>
2024-04-21 19:58:20 -0700

    python compile optimum neuron-TypeError: unsupported operand type(s) for //: 'str' and 'int'

--- app/compile-sd2.py
+++ app/compile-sd2.py
@@ -3,12 +3,13 @@ os.environ["NEURON_FUSE_SOFTMAX"] = "1"
 model_id=os.environ['MODEL_ID']
 device=os.environ["DEVICE"]
 model_dir=os.environ['COMPILER_WORKDIR_ROOT']
-height=os.environ['HEIGHT']
-width=os.environ['WIDTH']
+height=int(os.environ['HEIGHT'])
+width=int(os.environ['WIDTH'])
+batch_size=int(os.environ['BATCH_SIZE'])
 from optimum.neuron import NeuronStableDiffusionPipeline
 
 compiler_args = {"auto_cast": "matmul", "auto_cast_type": "bf16"}
-input_shapes = {"batch_size": 1, "height": height, "width": width}
+input_shapes = {"batch_size": batch_size, "height": height, "width": width}
 stable_diffusion = NeuronStableDiffusionPipeline.from_pretrained(model_id, export=True, **compiler_args, **input_shapes)
 stable_diffusion.save_pretrained(model_dir)
 
