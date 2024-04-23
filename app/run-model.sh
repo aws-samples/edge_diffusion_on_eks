a9d311417d5c3e409e26ff37b9808ce8c5c30234
Yahav Biran <yahavb@me.com>
2024-04-23 12:05:59 -0700

    inline_weights_to_neff-ModuleNotFoundError: No module named 'optimum'

--- app/run-model.sh
+++ app/run-model.sh
@@ -3,9 +3,9 @@ pip install --upgrade pip
 if [ "$(uname -i)" = "x86_64" ]; then
   if [ "$DEVICE" == "xla" ]; then
     #pip config set global.extra-index-url https://pip.repos.neuron.amazonaws.com
-    #pip install "optimum[neuronx, diffusers]"
-    pip install diffusers==0.20.2 transformers==4.33.1 accelerate==0.22.0 safetensors==0.3.1 matplotlib Pillow ipython -U      
-    pip install click
+    pip install "optimum[neuronx, diffusers]"
+    #pip install diffusers==0.20.2 transformers==4.33.1 accelerate==0.22.0 safetensors==0.3.1 matplotlib Pillow ipython -U      
+    #pip install click
     #pip install /diffusion_benchmarker-0.0.1.tar.gz 
     #diffusion_benchmarker t2i --pipeline neuronx_t2i root=/app/sd2_compile_dir_512 pretrained_model_name_or_path=stabilityai/stable-diffusion-2-1 torch_dtype=bfloat16
   elif [ "$DEVICE" == "cuda" ]; then
